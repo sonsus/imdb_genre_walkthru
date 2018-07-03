1e7769f23811d0afb20b6edec82c6aff405c55e8
SEONIL <seonilson92@gmail.com>
2018-07-03 16:47:13 +0900

    fucking ALTA

--- parser.py
+++ parser.py
@@ -10,46 +10,38 @@ from bs4 import BeautifulSoup
 #status = req.status_code
 #is_ok = req.ok
 
-def refine(entity):
+
+def refine(entity, instance):
     if entity == "genre_list":
-        genre_list = eval(entity)
-        for i, genre in enumerate(genre_list):
-            try: genre_list[i] = genre.contents[0].contents[0]
-            except: genre_list = genre_list[:-1]
-        return genre_list
-    elif entity == "runningtime":
-        runningtime = eval(entity)
-        runningtime = int(runningtime[0].attrs['datetime'][2:-1])
-        return runningtime
-    else: # for entity == year or title
-        res = eval(entity)[0].contents[0]
+        for i, genre in enumerate(instance): #instance == genre_list
+            try: instance[i] = instance.contents[0].contents[0] 
+            except: instance = instance_list[:-1]
+        return instance
+    elif entity == "runningtime": #instance == runningtime
+        instance = int(instance[0].attrs['datetime'][2:-1])
+        return instance
+    else: # title, year
+        res = instance[0].contents[0] #instance == year or title
         return res if entity=="year" else unicodedata.normalize("NFKD", res) 
 
-
-
-def title_yr_genre_runtime(moviekey):
+    
+    
+def title_yr_genre_runtime(self):
     req = requests.get("https://www.imdb.com/title/{moviekey}/".format(moviekey=moviekey))
     html = req.text
     soup = BeautifulSoup(html, "html.parser")
 
     title = soup.select(
-        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1", # CSSselector
-        )
-    year = soup.select(
-        "#titleYear > a",
-        )
-    genre_list = soup.select(
-        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div.subtext > a",
-        )
-    runningtime = soup.select(
-        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time",
-    )
+        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1",) # CSSselector
+    year = soup.select("#titleYear > a",)
+    genre_list = soup.select("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div.subtext > a",)
+    runningtime = soup.select("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time",)
 
     result_dict = {}
 
     for entity in ["title", "year", "genre_list", "runningtime"]:
-        result_dict[entity] = refine(eval(entity))
+        result_dict[entity] = refine(entity, eval(entity))
 
     return result_dict # {"title": ____, "year":____, "genre_list": [__, __, __, __, ..], "runningtime": ___(int in min)}
-        
-    
+
+
