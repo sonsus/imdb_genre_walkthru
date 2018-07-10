1cd0f40e720e5bd1892737b3116678f279a52bf0
SEONIL <seonilson92@gmail.com>
2018-07-10 16:45:56 +0900

    tt0244353 exception need to be overcame

--- .ipynb_checkpoints/parser-checkpoint.py
+++ .ipynb_checkpoints/parser-checkpoint.py
@@ -2,46 +2,52 @@ import requests
 #from pathlib import Path
 import unicodedata
 from bs4 import BeautifulSoup
+from IPython.core.debugger import set_trace
 
 
-#pwd = Path("/home/seonils/data_storage/story/")
-#datadir_json = pwd / "json" 
-#header = req.headers
-#status = req.status_code
-#is_ok = req.ok
 
-
-def refine(entity, instance):
-    if entity == "genre_list":
-        for i, genre in enumerate(instance): #instance == genre_list
-            try: instance[i] = instance.contents[0].contents[0] 
-            except: instance = instance_list[:-1]
-        return instance
-    elif entity == "runningtime": #instance == runningtime
-        instance = int(instance[0].attrs['datetime'][2:-1])
-        return instance
-    else: # title, year
-        res = instance[0].contents[0] #instance == year or title
-        return res if entity=="year" else unicodedata.normalize("NFKD", res) 
-
+class MovieInfo:
+    def __init__(self, moviekey):
+        #set_trace()
+        self.moviekey= moviekey
+        self.req = requests.get("https://www.imdb.com/title/{moviekey}/".format(moviekey=self.moviekey))
+        self.html = self.req.text
+        self.soup = BeautifulSoup(self.html, "html.parser")
+        
+        self.title = self.soup.select(
+        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1",) 
+        self.year = self.soup.select("#titleYear > a",) if not self.moviekey=="tt0244353" else \
+                    self.soup.select( "#title-episode-widget > div > div") # tt0244353 exception
+        self.genre_list = self.soup.select("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div.subtext > a",)
+        self.running_time = self.soup.select("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time",)
+        
+    def refine(self, entity):
+        if entity == "genre_list":
+            for i, genre in enumerate(self.genre_list): #instance == genre_list
+                try: self.genre_list[i] = self.genre_list[i].contents[0].contents[0] 
+                except: 
+                    if len(self.genre_list)>1: 
+                        self.genre_list = self.genre_list[:-1]
+            return self.genre_list
+        elif entity == "running_time": #instance == runningtime
+            self.running_time = int(self.running_time[0].attrs['datetime'][2:-1])
+            return self.running_time
+        elif entity == "title": # title, year
+            self.title = self.title[0].contents[0] #instance == year or title
+            return unicodedata.normalize("NFKD", self.title)
+        else: #entity == year
+            if self.moviekey != "tt0244353": res = self.year[0].contents[0]
+            else: res = self.year[3].contents[1].contents[0] # tt0244353 exception
+            self.year = res
+            return self.year
+        
+    def key_title_yr_genre_runtime(self):
+        result_dict = {}
+        result_dict["key"] = self.moviekey
+        for entity in ["title", "year", "genre_list", "running_time"]:
+            result_dict[entity] = self.refine(entity)
+        return result_dict 
     
-    
-def title_yr_genre_runtime(self):
-    req = requests.get("https://www.imdb.com/title/{moviekey}/".format(moviekey=moviekey))
-    html = req.text
-    soup = BeautifulSoup(html, "html.parser")
-
-    title = soup.select(
-        "#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > h1",) # CSSselector
-    year = soup.select("#titleYear > a",)
-    genre_list = soup.select("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div.subtext > a",)
-    runningtime = soup.select("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time",)
-
-    result_dict = {}
-
-    for entity in ["title", "year", "genre_list", "runningtime"]:
-        result_dict[entity] = refine(entity, eval(entity))
-
-    return result_dict # {"title": ____, "year":____, "genre_list": [__, __, __, __, ..], "runningtime": ___(int in min)}
+    # {"key": self.moviekey, "title": ____, "year":____, "genre_list": [__, __, __, __, ..], "runningtime": ___(int in min)}
 
 
